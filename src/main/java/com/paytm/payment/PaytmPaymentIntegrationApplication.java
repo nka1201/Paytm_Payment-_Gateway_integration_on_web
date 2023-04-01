eb75a5a6b7b2b687dbaa15940f6a621282707da1
nka1201 <129548944+nka1201@users.noreply.github.com>
2023-04-01 23:30:11 +0530

    Initial commit

---
+++ src/main/java/com/paytm/payment/PaytmPaymentIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.paytm.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmPaymentIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmPaymentIntegrationApplication.class, args);
+	}
+
+}
